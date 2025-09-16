3bdc82f8634fa3687a5106f6fd0f5e1c799cd00a
AM Student <amstudent@F251-16.local>
2025-09-16 08:14:46 -0500

    first

---
+++ Gardenclub/GardenclubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenclubApp.swift
+//  Gardenclub
+//
+//  Created by AM Student on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GardenclubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
